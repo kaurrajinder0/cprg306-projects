6a55c7515cd39c21400bc962e065f4e97d8677f6
kaurrajinder0 <141068515+kaurrajinder0@users.noreply.github.com>
2024-04-22 02:23:58 -0600

    i hope this workss!!!!

--- app/LinkResult.js
+++ app/LinkResult.js
@@ -1,15 +1,50 @@
 "use client"
+import axios from "axios";
 import React, { useEffect, useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 
+
 const LinkResult = ({inputValue}) => {
-    console.log(inputValue);
-    const [ShortenLink, setShortenLink] = useState("Hello World");
+    const [ShortenLink, setShortenLink] = useState("");
+    console.log(ShortenLink);
     const [copied, setCopied] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const fetchData = async () =>{
+        try{
+            setLoading(true);
+            const res= await axios(`https://api.urlshortener.com/shorten ${inputValue}`);
+            setShortenLink(res.data.result.full_short_link);
+
+        }catch(err) {
+            setError(err);
+        }finally {
+            setLoading(false);
+
+        }
+    }
+
+    useEffect(() =>{
+        if(inputValue.length){
+            fetchData();
+        }
+
+    }, [inputValue]);
+
+    if(loading){
+        return <p className="noData"> Loading...</p>
+    }
+
+    if(error){
+        return <p className="noData"> Something went wrong :(</p>
+    }
 
 
     return (
-        <div className='result flex flex-col items-center justify-center p-4 w-full max-w-md mx-auto'>
+        <>
+        {ShortenLink && (
+            <div className='result flex flex-col items-center justify-center p-4 w-full max-w-md mx-auto'>
             <p className="text-center">{copied ? "Copied!" : "Click button to copy the text"}</p>
             <CopyToClipboard 
                 text={ShortenLink}
@@ -27,6 +62,8 @@ const LinkResult = ({inputValue}) => {
                 </button>
             </CopyToClipboard>
         </div>
+            )}
+        </>
     );
 };
 
